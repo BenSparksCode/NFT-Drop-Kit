c3c7cee5b4601d521f09ac22332b9758540d4199
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-02-05 13:03:29 +0200

    Scaffold more test logic

--- test/MerkleTree.test.js
+++ test/MerkleTree.test.js
@@ -10,26 +10,59 @@ const { constants } = require("../utils/TestConstants");
 const {} = require("../utils/TestUtils");
 
 let owner, ownerAddress;
+let NFT;
 
 describe("Merkle Tree Tests", function () {
   beforeEach(async () => {
     [owner] = await ethers.getSigners();
 
     ownerAddress = await owner.getAddress();
+
+    const nftFactory = await ethers.getContractFactory("NFT");
+    NFT = await nftFactory.deploy(
+      "Teenage Mutant Ninja Turtles",
+      "TMNT",
+      "hiddenturtles.com",
+      "revealedturtles.com"
+    );
   });
 
-  it("5000 addresses merkle whitelisted, all can claim", async () => {
+  it("100 addresses merkle whitelisted, all can claim", async () => {
     //   TODO
 
-    const wallets = generateWallets(20);
+    // Generate 100 wallets and addresses
+    const wallets = generateWallets(100);
     const walletAddresses = wallets.map((w) => w.address);
 
-    console.log(walletAddresses);
-
+    // Create Merkle Root from 100 addresses for whitelist
     const merkleRoot = buildWhitelistMerkleRoot(walletAddresses);
-
     console.log(merkleRoot);
+
+    // TODO check merkleRoot is in correct bytes32 format
+
+    // Set Merkle Root in NFT as owner
+    await NFT.setWhitelistMerkleRoot(merkleRoot);
+
+    const evilWallets = generateWallets(10);
+
+    // loop through whitelisted addresses
+    for (let i = 0; i < wallets.length; i++) {
+      const currentWallet = wallets[i];
+
+      // claim NFT via whitelist mint function
+    }
+
+    // loop through evil wallets
+    for (let i = 0; i < evilWallets.length; i++) {
+      const currentEvilWallet = evilWallets[i];
+
+      // Check evil wallet not in whitelist wallet array
+
+      // Check evil wallet cannot mint NFT via whitelist function
+    }
   });
 
-  it;
+  it("5000 addresses merkle whitelisted, check all are on whitelist in view function", async () => {
+    // TODO
+  });
 });
