0d094646548106dcf87a61338ab5f4ddc4a04127
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-02-24 23:41:22 +0200

    Scaffold build merkle tree script

--- scripts/build_merkle_tree.js
+++ scripts/build_merkle_tree.js
@@ -1,1 +1,25 @@
-// TODO
+const hre = require("hardhat");
+const { ethers } = require("hardhat");
+require("@nomiclabs/hardhat-etherscan");
+const chalk = require("chalk");
+const fs = require("fs");
+const ProgressBar = require("progress");
+
+const { constants } = require("../utils/TestConstants");
+
+const writeToFile = (str) => {
+  // TODO
+};
+
+async function main() {
+  // TODO
+
+  console.log("✅✅  Merkle Tree Created! ✅✅");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
